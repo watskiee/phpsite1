fd6955835eeffbf50f39739cbcfc9f4d07f3e5d3
watskiee <sasha.ryba85@gmail.com>
2023-03-06 20:41:37 +0300

    ya

---
+++ site.php
@@ -0,0 +1,250 @@
+<div class=e201_2>
+  <div class=e1_7>
+    <div class="e1_5"></div><span  class="e1_6">One week only -50% off all courses with code 50OFF at checkout</span>
+  </div>
+  <div class=e1_8>
+    <div class="e1_9"></div><span  class="e1_12">Skillz</span>
+    <div class="e2_2"></div>
+    <div class="e2_6"></div>
+    <div class="e2_5"></div>
+    <div class="e2_4"></div><span  class="e2_7">Курсы</span><span  class="e2_8">Площадка</span><span  class="e2_9">Блог</span><span  class="e2_10">О  сайте</span><span  class="e2_11">Контакты</span>
+    <div class="e2_13"></div>
+    <div class="e2_14"></div>
+    <div class="e2_28"></div>
+    <div class="e2_38"></div>
+    <div class="e2_36"></div>
+    <div class="e2_39"></div><span  class="e2_40">0</span>
+  </div>
+  <div class="e3_2"></div>
+  <div class="e3_3"></div>
+  <div class="e3_5"></div>
+  <div class="e3_4"></div><span  class="e3_6">Featured Course</span><span  class="e3_7">Прокачай свои скиллы вместе со skillz !</span><span  class="e3_8">Уникальные курсы приведут вас к незабываемому результату)</span>
+  <div class="e3_9"></div><span  class="e3_10">Узнать больше</span>
+  <div class="e3_11"></div>
+  <div class="e3_12"></div><span  class="e3_13">Featured Courses</span><span  class="e3_14">Инновационные курсы</span><span  class="e3_15">Мы вас научим мыслить нестандартно и еще зарабатывать много денег</span>
+  <div class=e3_16>
+    <div class=e3_17>
+      <div class=e3_20>
+        <div class="e3_21"></div><span  class="e4_56">посмотреть все курсы</span>
+      </div>
+    </div>
+    <div class=e3_24>
+      <div class=e3_25>
+        <div class="e3_26"></div>
+        <div class=e3_27>
+          <div class="e3_28"></div><span  class="e3_29">Memphis Gex</span><span  class="e3_30">Узнать больше</span><span  class="e3_31">Как стать умнее в игре?</span><span  class="e3_32">$ 59.99 USD</span>
+        </div>
+        <div class="e3_33"></div>
+      </div>
+      <div class=e3_34>
+        <div class="e3_35"></div>
+        <div class=e3_36>
+          <div class="e3_37"></div><span  class="e3_38">Как зарабатывать на наскальной живописи?</span>
+          <div class=e3_39><span  class="e3_41">$ 39.99 USD</span></div><span  class="e3_42">Узнать больше</span><span  class="e3_43">Булатов Михаил</span>
+        </div>
+        <div class="e3_44"></div>
+      </div>
+      <div class=e3_45>
+        <div class="e3_46"></div>
+        <div class="e3_47"></div>
+        <div class=e3_48>
+          <div class="e3_49"></div><span  class="e3_50">Shao valo</span>
+          <div class=e3_51><span  class="e3_52">$ 39.99 USD</span></div><span  class="e3_54">Распишем вам план тренировок</span><span  class="e3_55">Узнать больше</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class=e7_2>
+    <div class="e7_3"></div>
+    <div class=e7_4>
+      <div class="e7_5"></div>
+      <div class=e7_6>
+        <div class="e7_7"></div><span  class="e7_8">Этим вопросом задаются сотни людей, обратитесь к нам за помощью</span>
+        <div class=e7_9>
+          <div class="e7_10"></div><span  class="e7_11">Узнать более</span>
+        </div><span  class="e7_12">Хотите стать жабой, но не знаете как?</span>
+      </div>
+    </div>
+  </div>
+  <div class=e7_13>
+    <div class=e7_14>
+      <div class="e7_15"></div>
+      <div class=e7_16>
+        <div class="e7_17"></div><span  class="e7_18">Посмотреть работы</span>
+      </div><span  class="e7_19">Бесплатная площадка по выкладке контента</span><span  class="e7_20">Бесплатно выкладывайте контент с жабами и получайте за это реальные деньги, просто оплатите подписку!</span>
+    </div>
+    <div class=e7_21>
+      <div class=e7_22>
+        <div class="e7_23"></div>
+        <div class=e7_24>
+          <div class="e7_25"></div><span  class="e7_26">Любой бизнес в интернете начинается с определения целевой аудитории.
+В интернете аудиторию определяют ее запросы — поисковые запросы в сети.</span><span  class="e7_28">Узнайте больше</span><span  class="e7_43">Бизнес?</span>
+        </div>
+      </div>
+      <div class=e7_29>
+        <div class="e7_30"></div>
+        <div class=e7_31>
+          <div class="e7_32"></div><span  class="e7_33">Как построить бизнес?</span><span  class="e7_34">Узнать более</span><span  class="e7_44">Когда человек решает открыть свое дело, он сталкивается с огромным количеством вопросов. Есть ли шанс на успех? Что такое бизнес-план и как его написать? Что выбрать: ИП или ООО? Как проще платить налоги? Рассказываем, что нужно знать начинающему предпринимателю и с чего начать, если хочешь открыть свое дело.</span>
+        </div>
+      </div>
+      <div class=e7_36>
+        <div class="e7_37"></div>
+        <div class=e7_38>
+          <div class="e7_39"></div><span  class="e7_40">процесс, включающий человека в социальную среду. Задача социальной рекламы является развитие у человека способности к самоконтролю и расширение социальных связей.</span><span  class="e7_41">Узнать более</span><span  class="e7_42">Социальная адаптация</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class=e7_45>
+    <div class="e7_46"></div>
+    <div class=e7_47>
+      <div class=e7_48><span  class="e7_60">«Курсы Александра Рыбалова  меняют правила игры. Он тщательно, организованный и объясняет вещи без излишеств, что позволяет любому — от новичка до эксперта — научиться чему-то на его курсах и вывести свою игру на новый уровень».</span><span  class="e7_61">Артём Селив, Influencer</span></div>
+      <div class=e7_62>
+        <div class="e7_63"></div>
+        <div class="e7_64"></div>
+      </div>
+    </div>
+    <div class=e8_65>
+      <div class=e8_66>
+        <div class="e8_67"></div>
+      </div>
+      <div class=e8_68>
+        <div class="e8_69"></div>
+      </div>
+      <div class=e8_70>
+        <div class="e8_71"></div>
+      </div>
+      <div class=e8_72>
+        <div class="e8_73"></div>
+      </div>
+      <div class=e8_74>
+        <div class="e8_75"></div>
+      </div>
+    </div>
+    <div class=e7_49>
+      <div class=e7_50>
+        <div class="e7_51"></div>
+      </div>
+      <div class=e7_52>
+        <div class="e7_53"></div>
+      </div>
+      <div class=e7_54>
+        <div class="e7_55"></div>
+      </div>
+      <div class=e7_56>
+        <div class="e7_57"></div>
+      </div>
+      <div class=e7_58>
+        <div class="e7_59"></div>
+      </div>
+    </div>
+  </div>
+  <div class=e8_76>
+    <div class="e8_77"></div>
+    <div class=e8_78>
+      <div class=e8_79>
+        <div class="e8_80"></div>
+        <div class="e8_95"></div>
+        <div class="e8_96"></div>
+        <div class="e8_97"></div>
+      </div>
+      <div class=e8_98>
+        <div class=e8_99>
+          <div class="e8_100"></div><span  class="e8_101">Все про жаб</span>
+        </div><span  class="e8_102">Просто жабка</span>
+        <div class="e8_103"></div><span  class="e8_104">Живет в речке и просто отдыхает</span>
+      </div>
+    </div>
+  </div>
+  <div class=e8_105>
+    <div class=e8_106>
+      <div class="e8_107"></div>
+      <div class=e8_108>
+        <div class="e8_109"></div><span  class="e8_110">View all articles</span>
+      </div><span  class="e8_111">Recent Articles</span><span  class="e8_112">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</span>
+    </div>
+    <div class=e8_113>
+      <div class=e8_114>
+        <div class="e8_115"></div>
+        <div class="e8_116"></div>
+        <div class=e8_117>
+          <div class="e8_118"></div><span  class="e8_119">Отгадайте ребус!</span><span  class="e8_120">February 8, 2022</span>
+        </div>
+      </div>
+      <div class=e8_122>
+        <div class="e8_123"></div>
+        <div class="e8_124"></div>
+        <div class=e8_125>
+          <div class="e8_126"></div><span  class="e8_127">February 8, 2022</span><span  class="e8_129">Сделаем фото на паспорт очень хорошо</span>
+        </div>
+      </div>
+      <div class=e8_130>
+        <div class="e8_131"></div>
+        <div class="e8_132"></div>
+        <div class=e8_133>
+          <div class="e8_134"></div><span  class="e8_135">Как фотографировать на телефоне?</span><span  class="e8_137">January 31, 2022</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class=e8_138>
+    <div class="e8_139"></div>
+    <div class=e8_140>
+      <div class="e8_141"></div>
+      <div class=e8_142>
+        <div class="e8_143"></div>
+        <div class="e8_144"></div><span  class="e8_145">Отправить</span>
+      </div><span  class="e8_146">Вы кузнец своего счастья. Все получится</span><span  class="e8_147">Ваша почта</span>
+    </div>
+  </div>
+  <div class=e8_148>
+    <div class="e8_149"></div>
+    <div class=e8_150>
+      <div class=e8_157><span  class="e8_168">Template</span></div>
+    </div>
+    <div class=e8_174>
+      <div class=e8_175>
+        <div class=e8_176>
+          <div class="e8_177"></div>
+          <div class=e8_178>
+            <div class="e8_179"></div>
+          </div>
+        </div>
+        <div class=e8_180>
+          <div class="e8_181"></div>
+          <div class=e8_182>
+            <div class="e8_183"></div>
+          </div>
+        </div>
+        <div class=e8_184>
+          <div class="e8_185"></div>
+          <div class=e8_186>
+            <div class=e8_187>
+              <div class="e8_188"></div>
+            </div>
+          </div>
+        </div>
+        <div class=e8_189>
+          <div class="e8_190"></div>
+          <div class=e8_191>
+            <div class="e8_192"></div>
+          </div>
+        </div>
+        <div class=e8_193>
+          <div class="e8_194"></div>
+          <div class=e8_195>
+            <div class="e8_196"></div>
+          </div>
+        </div>
+      </div><span  class="e8_197">Подписывайтесь на наши соц.сети</span>
+    </div>
+    <div class=e8_158>
+      <div class=e8_159><span  class="e8_160">Home</span><span  class="e8_161">Free Workshops</span><span  class="e8_162">About</span><span  class="e8_163">Courses</span><span  class="e8_164">Contact</span><span  class="e8_165">Blog</span></div><span  class="e8_166">learning Platform</span>
+    </div>
+    <div class=e8_169><span  class="e8_170">Style Guide</span><span  class="e8_171">Changelog</span><span  class="e8_172">Licenses</span><span  class="e8_173">Instructions</span></div><span  class="e11_2">Skillz</span>
+    <div class="e11_3"></div>
+    <div class="e11_4"></div>
+  </div>
+  <div class="e11_5"></div>
+  <div class="e11_6"></div>
+</div>
